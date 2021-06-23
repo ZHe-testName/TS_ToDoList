bef498b66d4fca1eec812d5242d8feacc4d88b91
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2021-06-23 11:57:08 +0300

    Incertted adding todo list test check

--- src/bll/todolist-reducer.test.ts
+++ src/bll/todolist-reducer.test.ts
@@ -1,19 +1,37 @@
-import { toDoListReducer, REMOVE_TODOLIST } from './todolist-reducer';
+import { toDoListReducer, REMOVE_TODOLIST, ADD_TODOLIST } from './todolist-reducer';
 import { ListsType } from './../App';
 import { v1 } from 'uuid';
         
         
-        test ('todo list should be removed', () => {
-            const toDoListId1 = v1(),
-                toDoListId2 = v1();
+test ('todo list should be removed', () => {
+    const toDoListId1 = v1(),
+        toDoListId2 = v1();
 
-            const startStage: Array<ListsType> = [
-                {id: toDoListId1, title: 'What to learn', filter: 'all'},
-                {id: toDoListId2, title: 'What to do', filter: 'all'},
-            ];
+    const startStage: Array<ListsType> = [
+        {id: toDoListId1, title: 'What to learn', filter: 'all'},
+        {id: toDoListId2, title: 'What to do', filter: 'all'},
+    ];
 
-            const endStage = toDoListReducer(startStage, {type: REMOVE_TODOLIST, tergetId: toDoListId1});
+    const endStage = toDoListReducer(startStage, {type: REMOVE_TODOLIST, tergetId: toDoListId1});
 
-            expect(endStage.length).toBe(1);
-            expect(endStage[0].id).toBe(toDoListId2);
-        });
+    expect(endStage.length).toBe(1);
+    expect(endStage[0].id).toBe(toDoListId2);
+});
+
+test ('correct todo list should be added', () => {
+    const toDoListId1 = v1(),
+        toDoListId2 = v1();
+
+    const newToDoListTitle = 'Newe TiDi List';
+
+    const startStage: Array<ListsType> = [
+        {id: toDoListId1, title: 'What to learn', filter: 'all'},
+        {id: toDoListId2, title: 'What to do', filter: 'all'},
+    ];
+
+    const endStage = toDoListReducer(startStage, {type: ADD_TODOLIST, newTitle: newToDoListTitle});
+
+    expect(endStage.length).toBe(3);
+    expect(endStage[2].title).toBe(newToDoListTitle);
+    expect(endStage[2].filter).toBe('all');
+});
--- src/bll/todolist-reducer.ts
+++ src/bll/todolist-reducer.ts
@@ -1,6 +1,8 @@
+import { v1 } from 'uuid';
 import { ListsType } from "../App";
 
-export const REMOVE_TODOLIST = 'REMOVE_TODOLIST';
+export const REMOVE_TODOLIST = 'REMOVE_TODOLIST',
+    ADD_TODOLIST = 'ADD_TODOLIST';
 
 type ActionType = {
     type: string,
@@ -10,8 +12,20 @@ type ActionType = {
 export const toDoListReducer = (state: Array<ListsType>, action: ActionType): Array<ListsType> => {
     switch (action.type){
         case REMOVE_TODOLIST:
- 
+            return state.filter(list => list.id !== action.tergetId);
 
+        case ADD_TODOLIST: 
+            const newToDoList: ListsType = {
+                id: v1(),
+                title: action.newTitle,
+                filter: 'all',
+            };
+
+            return [
+                ...state,
+                newToDoList,
+            ];
+            
         default:
             throw new Error('I dont understand this action type.');
     };
