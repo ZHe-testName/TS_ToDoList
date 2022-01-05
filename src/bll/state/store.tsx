bb4fff4c923fb5e2af1ba2d06dae6e62db25714d
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2022-01-05 10:54:44 +0200

    Added redux-toolkit. Fixed faults with it. Yeah!!!

--- src/bll/state/store.tsx
+++ src/bll/state/store.tsx
@@ -1,9 +1,10 @@
-import { combineReducers, createStore, applyMiddleware } from "redux";
+import { combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { appReducer } from "../app-reducer/app-reducer";
 import authReducer from "../auth-reducer/auth-reducer";
 import { taskReducer } from "../task-reducer/task-reducer";
 import { toDoListReducer } from "../todolist-reducer/todolist-reducer";
+import { configureStore } from "@reduxjs/toolkit";
 
 //создаем общий рутовый редюсер для редакс
 
@@ -18,10 +19,28 @@ const rootReducer = combineReducers({
 
 //создаем новый стор на основе редюсеров
 //добавляем промежуточный слой для thunk
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+// export const store = createStore(rootReducer, applyMiddleware(thunk));
+
+//для использования rdux toolkit нужно его подключить
+//он задает нам одинаковый стиль написания проектов
+//он берет на себя все подключения редюсерров
+//настройку типизации и прочее
+
+//это как бы надстройка обертка над стором редакса
+
+//для создания стора используется функция configureStore
+//и в нее передается объект с настройками: наши редюсеры, помежуточные слои прочее...
+
+export const store = configureStore({
+    reducer: rootReducer,
+
+    //для типизации редюсера в настройку промижуточного слоя передается функция
+    //которая получить дефолтный промежуточный слой и добавляет в начало или конец наши собственные промежуточные слои
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunk),
+});
 
 //cздаем на  автоматически тип всего объекта состояня
-export type AppRootStateType = ReturnType<typeof rootReducer>;
+export type AppRootStateType = ReturnType<typeof store.getState>;
 
 //для работы со стором с консоли
 //создадим глобальную переменную с сылкой на стор
